cf197927f119b079a0a4f22f3987cea145bbeb79
Trung Hieu Tran <hieutt.bka59@gmail.com>
2019-04-22 23:31:20 +0700

    first commit

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('recharge', 'RechargeController@create');
+Route::get('recharge', 'RechargeController@create')->name('recharge.create')->middleware('jwt');
 
